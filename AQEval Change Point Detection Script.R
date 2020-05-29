df00f518bd6e5e0052d038126b9f6f4144acfd10
airqualityanthony <walker.anthony91@gmail.com>
2020-05-29 15:49:36 +0100

    Create AQEval Change Point Detection Script.R

---
+++ AQEval Change Point Detection Script.R
@@ -0,0 +1,42 @@
+## AQEval Change point detection
+library(AQEval)
+library(openair)
+library(ggplot2)
+library(dplyr)
+
+AQEvalMultipleSite <- function(site, years){
+  data <- importAURN(site = site, year = years)
+  
+  data$iso <- AQEval::isolateContribution(data = data, pollutant = "no2")
+  
+  
+  data_daily <- timeAverage(data,avg.time = "day")
+  
+  
+  bps <- AQEval::findBreakPoints(data_daily, pollutant = "iso")
+  bps$site <- site
+  
+  quant <- quantBreakPoints(data_daily,"iso",breaks = bps)
+  
+  output <- list(bps=bps,plot = quant$plot, report = quant$report)
+  return(output)
+}
+
+meta_data <- importMeta(all = TRUE) %>% 
+  filter(variable == "NO2", end_date == "ongoing",start_date <= "2018-01-01", site_type == "Urban Traffic")
+
+aqeval_output <- list()
+
+
+for (i in meta_data$code){
+  aqeval_output[[length(aqeval_output)+1]] <- AQEvalMultipleSite(i,years = 2019:2020)
+}
+
+## return all model stats
+aqeval_stats <- data.frame()
+
+for (i in seq(1,length(aqeval_output),1)){
+  aqeval_stats <- rbind(aqeval_stats,as.data.frame(aqeval_output[[i]][1]))
+}
+
+
